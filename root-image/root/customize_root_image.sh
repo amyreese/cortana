8abce08a5409003a7fd19e67893aa554e30e44b3
John Reese <john@noswap.com>
2013-07-06 20:37:06 -0700

    Fix password back to 'cortana'

--- root-image/root/customize_root_image.sh
+++ root-image/root/customize_root_image.sh
@@ -10,7 +10,7 @@ ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 usermod -s /usr/bin/zsh root
 cp -aT /etc/skel/ /root/
 
-useradd -s /bin/zsh -m -G wheel -p '$6$vRNJ3AdP$V3d0O3Nyn4MW6E2O/ajWNc6dA9deezM9TVPHO1xbMGIEU3EYpv6HItBj0DDNW4c0YdksoN4/zbEGe4jE/ujPl0' jreese
+useradd -s /bin/zsh -m -G wheel -p '$6$JCGVeMa8$stdvt1VdcfyAjZbKFR52rwy9rOmCxhT98iKZfi8VzZHK8Kjwn5OsR66UGTqaTfx9QrDJV2rsNxeVa8ZuHVUOO.' jreese
 
 chmod 750 /etc/sudoers.d
 chmod 440 /etc/sudoers.d/g_wheel
